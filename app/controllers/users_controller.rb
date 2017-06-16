c0e9d90891c7336cbce7165865f50e71d425e216
monzelb <monzelb@gmail.com>
2017-06-16 09:57:18 -0700

    not much

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < ApplicationController
     github = Github.new basic_auth: "monzelb:#{Rails.application.secrets.github_key}"
     @query = params[:query]
     @followers = github.users.followers.list "#{@query}"
-    p @followers
 
     if @query
       respond_to do |format|
