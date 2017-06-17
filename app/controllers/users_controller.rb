9bffc9b5e855ba5690a94519fcfe24fe2f9e7310
monzelb <monzelb@gmail.com>
2017-06-16 17:22:30 -0700

    got things back to normal

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,17 @@
 class UsersController < ApplicationController
   def search
-    github = Github.new basic_auth: "monzelb:#{Rails.application.secrets.github_key}"
+    # github = Github.new basic_auth: "monzelb:#{Rails.application.secrets.github_key}"
+    github = Octokit::Client.new(:login => 'monzelb', :password => "#{Rails.application.secrets.github_key}")
     @query = params[:query]
-    @followers = github.users.followers.list "#{@query}"
-
+    @followers = github.followers("#{@query}")
+    @blah = "blah"
+    gon.blah = @blah
+    # @followers = client.users.followers.list "#{@query}"
     if @query
       respond_to do |format|
         format.js {}
         format.html {redirect_to search_url}
       end
     end
-
   end
 end
