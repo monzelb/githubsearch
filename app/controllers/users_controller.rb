85b8df7b1c47ab0602344a10e63097f12bc84715
monzelb <monzelb@gmail.com>
2017-06-19 10:03:07 -0700

    removed gon stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,8 @@ class UsersController < ApplicationController
   def search
     github = Octokit::Client.new(:login => 'monzelb', :password => ENV['github_key'])
     @query = params[:query]
-    gon.query = @query
     @followers = github.followers("#{@query}")
-    gon.watch.followers = @followers
     @blah = "blah"
-    gon.blah = @blah
     if @query
       respond_to do |format|
         format.js { render "search", :locals => {:query => @query} }
